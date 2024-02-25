be327edef05a6c532b457bf61ffe706bb24886a0
morning810 <failnever263@gmail.com>
2024-02-26 01:14:41 +0900

    second commit

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://tokentrendingbot.org/api"
+export const BACKEND_URL = "https://127.0.0.1:27017/api"
 export const ALCHEMY_KEY = "01n5OZDGP2J0EtPIrrTZE5inAE-qRWPH"
